a979750cb73baf06c7085a3c8e020e94d0be5740
MishaTarasenko <misha0106@gmail.com>
2023-06-07 23:15:26 +0300

    а

--- first.js
+++ first.js
@@ -124,7 +124,7 @@ function addProduct(){
 function checkName(productName){
    const allProducts = document.querySelectorAll(".label");
    for(let i = 0; i < allProducts.length; i++){
-      if(productName.toLowerCase() == allProducts[i].textContent.toLowerCase())
+      if(productName.toLowerCase() === allProducts[i].textContent.toLowerCase())
          return false;
    }
    return true;
@@ -162,7 +162,7 @@ function minusButtonClicked(button){
    if(amount > 1){
       amount = amount - 1;
       blockOfAmount.textContent = amount;
-      if(amount == 1)
+      if(amount === 1)
          button.setAttribute("class" , "minusButton disabled");
    }
    let name = blockOfButtons.parentNode.querySelector(".label").textContent;
@@ -172,7 +172,7 @@ function minusButtonClicked(button){
    
    for(let i = 0; i < items.length; i++){
       let itemName = items[i].querySelector(".itemName").textContent;
-      if(itemName == name){
+      if(itemName === name){
          let amountOfItem = items[i].querySelector(".amountOfItem");
          amountOfItem.textContent = amount;
          break;
@@ -197,7 +197,7 @@ function plusButtonClicked(button){
    
    for(let i = 0; i < items.length; i++){
       let itemName = items[i].querySelector(".itemName").textContent;
-      if(itemName == name){
+      if(itemName === name){
          let amountOfItem = items[i].querySelector(".amountOfItem");
          amountOfItem.textContent = amount;
          break;
@@ -230,7 +230,7 @@ function labelClicked(button){
    
             for(let i = 0; i < items.length; i++){
                let itemName = items[i].querySelector(".itemName");
-               if(currentName == itemName.textContent){
+               if(currentName === itemName.textContent){
                   let item = itemName.parentNode;
                   item.removeChild(itemName);
                   let newItemName = document.createElement("article");
@@ -307,7 +307,7 @@ function buyButtonClicked(button){
    
    for(let i = 0; i < items.length; i++){
       let itemName = items[i].querySelector(".itemName");
-      if(name == itemName.textContent){
+      if(name === itemName.textContent){
          productStatistic.removeChild(items[i]);
 
          let productStatisticBought = statistic.querySelector(".productStatistic.bought");
@@ -421,7 +421,7 @@ function unpurchasedButtonClicked(button){
    
    for(let i = 0; i < items.length; i++){
       let itemName = items[i].querySelector(".itemName");
-      if(name == itemName.textContent){
+      if(name ===itemName.textContent){
          productStatisticBought.removeChild(items[i]);
 
          let productStatistic = statistic.querySelector(".productStatistic");
@@ -456,7 +456,7 @@ function cancelButtonClicked(button){
    
    for(let i = 0; i < items.length; i++){
       let itemName = items[i].querySelector(".itemName");
-      if(name == itemName.textContent){
+      if(name === itemName.textContent){
          productStatisticBought.removeChild(items[i]);
          break;
       }
@@ -467,9 +467,197 @@ function cancelButtonClicked(button){
    
    for(let i = 0; i < items.length; i++){
       let itemName = items[i].querySelector(".itemName");
-      if(name == itemName.textContent){
+      if(name === itemName.textContent){
          productStatistic.removeChild(items[i]);
          break;
       }
    }
 }
+
+function saveData() {
+   const data = getIncomeRows().map((row) => {
+     if (!row.querySelector(".button bought")) {
+       return {
+         name: row.querySelector(".label").textContent.trim(),
+         amount: row.querySelector(".blockOfAmount").textContent,
+         buttonText: row.querySelector(".button").textContent.trim(),
+         isSold: false,
+       };
+     } else {
+       return {
+         name: row.querySelector(".label crossedOut").textContent.trim(),
+         amount: row.querySelector(".blockOfAmount").value,
+         buttonText: row.querySelector(".button bought").textContent.trim(),
+         isSold: true,
+       };
+     }
+   });
+ 
+   localStorage.setItem("products", JSON.stringify(data));
+ }
+ 
+ function getIncomeRows() {
+   return Array.from(document.querySelectorAll(".products .product"));
+ }
+
+function getProducts() {
+   let products;
+   if (localStorage.getItem("products") === null) {
+     products = [];
+   } else {
+     products = JSON.parse(localStorage.getItem("products"));
+   }
+   products.forEach(function (product) {
+      let nameOfProduct = document.createElement("section");
+      nameOfProduct.setAttribute("class", "nameOfProduct");
+      let name = document.createElement("p");
+      if (!product.isSold) {
+         name.setAttribute("class", "label");
+      } else {
+         name.setAttribute("class", "label crossedOut");
+      }
+      name.textContent = productName;
+      nameOfProduct.appendChild(name);
+      product.appendChild(nameOfProduct);
+
+      let buttonGroupOne = document.createElement("section");
+      buttonGroupOne.setAttribute("class", "buttonBlockOne");
+ 
+      if (!product.isSold) {
+       let minusButton = document.createElement("button");
+       if (product.amount > 1) {
+         minusButton.setAttribute("class", "minusButton");
+       } else {
+         minusButton.setAttribute("class", "minusButton disabled");
+       }
+       minusButton.textContent = "–";
+
+      let tooltipSectionMinus = document.createElement("section");
+      tooltipSectionMinus.setAttribute("class", "tooltipSection");
+      let labelTooltipMinus = document.createElement("label");
+      labelTooltipMinus.setAttribute("data-tooltip", "tooltip");
+      labelTooltipMinus.setAttribute("for", "tooltip");
+      labelTooltipMinus.textContent = "Зменшити кількість";
+      tooltipSectionMinus.appendChild(labelTooltipMinus);
+      minusButton.appendChild(tooltipSectionMinus);
+
+      let blockOfAmount = document.createElement("section");
+      blockOfAmount.setAttribute("class", "blockOfAmount");
+      blockOfAmount.textContent = product.amount;
+ 
+      let plusButton = document.createElement("button");
+      plusButton.setAttribute("class", "plusButton");
+      plusButton.textContent = "+";
+
+      let tooltipSectionPlus = document.createElement("section");
+      tooltipSectionPlus.setAttribute("class", "tooltipSection");
+      let labelTooltipPlus = document.createElement("label");
+      labelTooltipPlus.setAttribute("data-tooltip", "tooltip");
+      labelTooltipPlus.setAttribute("for", "tooltip");
+      labelTooltipPlus.textContent = "Збільшити кількість";
+      tooltipSectionPlus.appendChild(labelTooltipPlus);
+      plusButton.appendChild(tooltipSectionPlus);
+
+      buttonGroupOne.appendChild(minusButton);
+      buttonGroupOne.appendChild(blockOfAmount);
+      buttonGroupOne.appendChild(plusButton);
+      product.appendChild(buttonGroupOne);
+
+
+
+      let buttonBlockTwo = document.createElement("section");
+      buttonBlockTwo.setAttribute("class", "buttonBlockTwo");
+      let buyButton = document.createElement("button");
+      buyButton.setAttribute("class", "button");
+      buyButton.textContent = "Куплено";
+
+      let tooltipSectionBuy = document.createElement("section");
+      tooltipSectionBuy.setAttribute("class", "tooltipSection buy");
+      let labelTooltipBuy = document.createElement("label");
+      labelTooltipBuy.setAttribute("data-tooltip", "tooltip");
+      labelTooltipBuy.setAttribute("for", "tooltip");
+      labelTooltipBuy.textContent = "Купити";
+      tooltipSectionBuy.appendChild(labelTooltipBuy);
+      buyButton.appendChild(tooltipSectionBuy);
+
+      let cancelButton = document.createElement("button");
+      cancelButton.setAttribute("class", "cancelButton");
+      cancelButton.textContent = "\u2716";
+
+      let tooltipSectionCancel = document.createElement("section");
+      tooltipSectionCancel.setAttribute("class", "tooltipSection cancel");
+      let labelTooltipCancel = document.createElement("label");
+      labelTooltipCancel.setAttribute("data-tooltip", "tooltip");
+      labelTooltipCancel.setAttribute("for", "tooltip");
+      labelTooltipCancel.textContent = "Відмінити";
+      tooltipSectionCancel.appendChild(labelTooltipCancel);
+      cancelButton.appendChild(tooltipSectionCancel);
+
+      buttonBlockTwo.appendChild(buyButton);
+      buttonBlockTwo.appendChild(cancelButton);
+      product.appendChild(buttonBlockTwo);
+   
+      products.appendChild(product);
+     } else {
+      let blockOfAmount = document.createElement("section");
+      blockOfAmount.setAttribute("class", "blockOfAmount");
+      blockOfAmount.textContent = product.amount;
+ 
+      buttonGroupOne.appendChild(amountInput);
+ 
+      let buttonBlockTwo = document.createElement("section");
+      buttonBlockTwo.setAttribute("class", "buttonBlockTwo");
+      let buyButton = document.createElement("button");
+      buyButton.setAttribute("class", "button bought");
+      buyButton.textContent = " Не Куплено";
+
+      let tooltipSectionBought = document.createElement("section");
+      tooltipSectionBought.setAttribute("class", "tooltipSection bought");
+      let labelTooltipBought = document.createElement("label");
+      labelTooltipBought.setAttribute("data-tooltip", "tooltip");
+      labelTooltipBought.setAttribute("for", "tooltip");
+      labelTooltipBought.textContent = "Не куплено";
+      tooltipSectionBought.appendChild(labelTooltipBought);
+      buyButton.appendChild(tooltipSectionBought);
+
+      buttonBlockTwo.appendChild(buyButton);
+      product.appendChild(buttonBlockTwo);
+     }
+ 
+     document.querySelector(".operations-div").appendChild(newProduct);
+ 
+     if (product.isSold) {
+      let productStatisticBought = statistic.querySelector(".productStatistic.bought");
+      let boughtItem = document.createElement("section");
+      let itemName = document.createElement("article");
+      itemName.setAttribute("class", "itemName");
+      let amountOfItem = document.createElement("section");
+      boughtItem.setAttribute("class", "item bought");
+      itemName.textContent = product.name;
+      amountOfItem.setAttribute("class", "amountOfItem");
+      amountOfItem.textContent = product.amount;
+
+      boughtItem.appendChild(itemName);
+      boughtItem.appendChild(amountOfItem);
+     } else {
+      let productStatistic = statistic.querySelector(".productStatistic");
+      let item = document.createElement("section");
+      let itemName = document.createElement("article");
+      itemName.setAttribute("class", "itemName");
+      let amountOfItem = document.createElement("section");
+      item.setAttribute("class", "item");
+      itemName.textContent = product.name;
+      amountOfItem.setAttribute("class", "amountOfItem");
+      amountOfItem.textContent = product.amount;
+
+      item.appendChild(itemName);
+      item.appendChild(amountOfItem);
+
+      productStatistic.appendChild(item);
+     }
+   });
+   //allAvailiableProduct = document.querySelectorAll(".products");
+   //fillWithDefaultValues();
+ }
+ 
+ document.addEventListener("DOMContentLoaded", getProducts);
